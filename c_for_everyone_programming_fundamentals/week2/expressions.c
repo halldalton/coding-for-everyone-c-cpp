2bb9217c3ee7c0c9fe7e34e5a7093ba32b785964
halldalton <halldalton94@outlook.com>
2021-07-10 16:53:15 -0500

    fixed indentation

--- c_for_everyone_programming_fundamentals/week2/expressions.c
+++ c_for_everyone_programming_fundamentals/week2/expressions.c
@@ -16,23 +16,23 @@ int main(void)
 
     c = b - a;
     printf("a = %d, b = %d, c = %d, d = %d\n",
-        a, b, c, d);
+           a, b, c, d);
 
     c = a / b; d = b / a;
     printf("a = %d, b = %d, c = %d, d = %d\n",
-        a, b, c, d);
+           a, b, c, d);
 
     c = a % b; d = b % a;
     printf("a = %d, b = %d, c = %d, d = %d\n",
-        a, b, c, d);
+           a, b, c, d);
 
     c = -a - b; d = -b - a;
     printf("a = %d, b = %d, c = %d, d = %d\n",
-        a, b, c, d);
+           a, b, c, d);
 
     c = ++a + b++; d += 5;
     printf("a = %d, b = %d, c = %d, d = %d\n",
-        a, b, c, d);
+           a, b, c, d);
 
     return 0;
 }
